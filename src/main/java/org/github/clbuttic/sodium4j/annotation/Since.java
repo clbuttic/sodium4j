f1f8e7966b270075e83366d713d8cb33255596a6
clbuttic <1764740+clbuttic@users.noreply.github.com>
2019-02-12 09:45:34 +0200

    Add @Documented annotation so annotations appear in JavaDoc.

--- src/main/java/org/github/clbuttic/sodium4j/annotation/Since.java
+++ src/main/java/org/github/clbuttic/sodium4j/annotation/Since.java
@@ -1,15 +1,13 @@
 package org.github.clbuttic.sodium4j.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * An annotation used to decorate SodiumLibrary and indicate when a function was introducted into libSodium.
  *
  * This helps backwards compatibility, by preventing calls to non-existent functions.
  */
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Since {
