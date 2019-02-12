930c65a4837944eb93ddbbafaa2856d79a02fe6b
clbuttic <1764740+clbuttic@users.noreply.github.com>
2019-02-12 10:03:12 +0200

    Allow @Since on classes.

--- src/main/java/org/github/clbuttic/sodium4j/annotation/Since.java
+++ src/main/java/org/github/clbuttic/sodium4j/annotation/Since.java
@@ -9,7 +9,7 @@ import java.lang.annotation.*;
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD, ElementType.TYPE})
 public @interface Since {
     public String value();
 }
